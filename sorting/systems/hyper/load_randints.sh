b1fb12dd5005cf8bcddc3fd21046fd70987632fd
Laurens Kuiper <laurens.kuiper@cwi.nl>
2021-07-22 16:15:57 +0200

    fix error in loading script

--- sorting/systems/hyper/load_randints.sh
+++ sorting/systems/hyper/load_randints.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . ../../pathvar.sh
 
-sudo docker exec -it hyper-container psql -U raasveld -p 7484 -h localhost test -f /sorting_data/randints/schema/schema.sql
-sudo docker exec -it hyper-container psql -U raasveld -p 7484 -h localhost test -f /sorting_data/randints/load/load.sql
+sudo docker exec -i hyper-container psql -U raasveld -p 7484 -h localhost test -f /sorting_data/randints/schema/schema.sql
+sudo docker exec -i hyper-container psql -U raasveld -p 7484 -h localhost test -f /sorting_data/randints/load/load.sql
