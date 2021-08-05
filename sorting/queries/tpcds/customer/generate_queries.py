d1ccb90da6687800b62abe170a10be1987c72505
Laurens Kuiper <laurens.kuiper@cwi.nl>
2021-08-05 14:55:56 +0200

    change customer queries

--- sorting/queries/tpcds/customer/generate_queries.py
+++ sorting/queries/tpcds/customer/generate_queries.py
@@ -21,23 +21,40 @@ varchar_columns = [
     'c_email_address'
 ]
 
-int_col_idxs = [12, 13, 14, 1, 3, 4, 5, 6, 7, 18]
-varchar_col_idxs = [2, 8, 9, 10, 11, 15, 16, 17]
-
-# increase the amount of sorting columns
+# increase the amount of int sorting columns
 for i in range(1, len(int_columns) + 1):
     with open(f'sql/int{i}.sql', 'w+') as f:
         print('CREATE TEMPORARY TABLE output AS SELECT * FROM customer ORDER BY ' + ', '.join(int_columns[:i]) + ';', file=f)
     with open(f'clickhouse/int{i}.sql', 'w+') as f:
         print('CREATE TABLE output ENGINE = File(Native) AS SELECT * FROM customer ORDER BY ' + ', '.join(int_columns[:i]) + ';', file=f)
-    with open(f'gnu/int{i}.sh', 'w+') as f:
-        print('sort -t, ' + ' '.join([f'-k{col_idx},{col_idx}n' for col_idx in int_col_idxs[:i]]) + ' ../../data/tpcds/sf$sf$/data/22_customer.csv', file=f, end='')
 
-# increase the amount of sorting columns
+# increase the amount of varchar sorting columns
 for i in range(1, len(varchar_columns) + 1):
     with open(f'sql/varchar{i}.sql', 'w+') as f:
         print('CREATE TEMPORARY TABLE output AS SELECT * FROM customer ORDER BY ' + ', '.join(varchar_columns[:i]) + ';', file=f)
     with open(f'clickhouse/varchar{i}.sql', 'w+') as f:
         print('CREATE TABLE output ENGINE = File(Native) AS SELECT * FROM customer ORDER BY ' + ', '.join(varchar_columns[:i]) + ';', file=f)
-    with open(f'gnu/varchar{i}.sh', 'w+') as f:
-        print('sort -t, ' + ' '.join([f'-k{col_idx},{col_idx}n' for col_idx in varchar_col_idxs[:i]]) + ' ../../data/tpcds/sf$sf$/data/22_customer.csv', file=f, end='')
+
+# increase the amount of int payload columns
+for i in range(1, len(int_columns) + 1):
+    with open(f'sql/int_payload{i}.sql', 'w+') as f:
+        print('CREATE TEMPORARY TABLE output AS SELECT ' + ', '.join(int_columns[:i]) + ' FROM customer ORDER BY c_birth_year, c_birth_month, c_birth_day;', file=f)
+    with open(f'clickhouse/int_payload{i}.sql', 'w+') as f:
+        print('CREATE TABLE output ENGINE = File(Native) AS SELECT ' + ', '.join(int_columns[:i]) + ' FROM customer ORDER BY c_birth_year, c_birth_month, c_birth_day;', file=f)
+
+# increase the amount of varchar payload columns
+for i in range(1, len(varchar_columns) + 1):
+    with open(f'sql/varchar_payload{i}.sql', 'w+') as f:
+        print('CREATE TEMPORARY TABLE output AS SELECT ' + ', '.join(varchar_columns[:i]) + ' FROM customer ORDER BY c_birth_year, c_birth_month, c_birth_day;', file=f)
+    with open(f'clickhouse/varchar_payload{i}.sql', 'w+') as f:
+        print('CREATE TABLE output ENGINE = File(Native) AS SELECT ' + ', '.join(varchar_columns[:i]) + ' FROM customer ORDER BY c_birth_year, c_birth_month, c_birth_day;', file=f)
+
+with open('sql/sort_strings.sql', 'w+') as f:
+    print('CREATE TEMPORARY TABLE output AS SELECT * FROM customer ORDER BY c_first_name, c_last_name;', file=f)
+with open('clickhouse/sort_strings.sql', 'w+') as f:
+    print('CREATE TEMPORARY TABLE output ENGINE = File(Native) AS SELECT * FROM customer ORDER BY c_first_name, c_last_name;', file=f)
+
+with open('sql/sort_ints.sql', 'w+') as f:
+    print('CREATE TEMPORARY TABLE output AS SELECT * FROM customer ORDER BY c_birth_year, c_birth_month, c_birth_day;', file=f)
+with open('clickhouse/sort_ints.sql', 'w+') as f:
+    print('CREATE TEMPORARY TABLE output ENGINE = File(Native) AS SELECT * FROM customer ORDER BY c_birth_year, c_birth_month, c_birth_day;', file=f)
