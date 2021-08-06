068ae209e095ec228ef250d2e2cd2fca13cda5e1
Laurens Kuiper <laurens.kuiper@cwi.nl>
2021-08-06 10:04:31 +0200

    fix indentation

--- sorting/systems/duckdb/randints_client.py
+++ sorting/systems/duckdb/randints_client.py
@@ -5,39 +5,39 @@ import time
 from tqdm import tqdm
 
 def run(con, query_folder, results_folder):
-	for qname in tqdm(os.listdir(query_folder)):
-		# skip .keep file
-		if not qname.endswith('.sql'):
-			continue
+    for qname in tqdm(os.listdir(query_folder)):
+        # skip .keep file
+        if not qname.endswith('.sql'):
+            continue
 
-		# skip if already done
-		if (os.path.isfile(results_folder + qname)):
-			continue
+        # skip if already done
+        if (os.path.isfile(results_folder + qname)):
+            continue
 
-		# read the query
-		with open(query_folder + qname, 'r') as f:
-			query = f.read()
+        # read the query
+        with open(query_folder + qname, 'r') as f:
+            query = f.read()
 
-		# time and execute the query
-		for i in range(5):
-			con.execute('DROP TABLE IF EXISTS output;')
+        # time and execute the query
+        for i in range(5):
+            con.execute('DROP TABLE IF EXISTS output;')
 
-			before = time.time()
-			con.execute(query)
-			after = time.time()
+            before = time.time()
+            con.execute(query)
+            after = time.time()
 
-			# write time to csv
-			with open(results_folder + 'results.csv', 'a+') as f:
-				print(qname.split('.')[0] + f',{after - before}', file=f)
+            # write time to csv
+            with open(results_folder + 'results.csv', 'a+') as f:
+                print(qname.split('.')[0] + f',{after - before}', file=f)
 
-		# create empty file to mark query as done
-		open(results_folder + qname, 'w+')
+        # create empty file to mark query as done
+        open(results_folder + qname, 'w+')
 
 def main():
-	con = duckdb.connect('randints.db')
-        con.execute("PRAGMA threads=8; PRAGMA memory_limit='70GB'") 
-	run(con, '../../queries/randints/sql/', '../../results/duckdb/randints/')
-	run(con, '../../queries/randints/duckdb/', '../../results/duckdb/randints_threads/')
+    con = duckdb.connect('randints.db')
+    con.execute("PRAGMA threads=8; PRAGMA memory_limit='70GB'") 
+    run(con, '../../queries/randints/sql/', '../../results/duckdb/randints/')
+    run(con, '../../queries/randints/duckdb/', '../../results/duckdb/randints_threads/')
 
 if __name__ == '__main__':
-	main()
+    main()
