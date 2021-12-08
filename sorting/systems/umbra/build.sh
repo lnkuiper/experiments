650f9cd03fc570c508f852ebbd0a09cece70880c
Laurens Kuiper <laurens.kuiper@.cwi.nl>
2021-12-08 17:58:53 +0100

    re-add umbra

--- sorting/systems/umbra/build.sh
+++ sorting/systems/umbra/build.sh
@@ -6,5 +6,5 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd umbra-binaries
-sudo docker build . --tag umbra-image
+docker build . --tag umbra-image
 
--- sorting/systems/umbra/start.sh
+++ sorting/systems/umbra/start.sh
@@ -2,7 +2,7 @@
 . ../../pathvar.sh
 
 # Add --cpuset-cpus=0 to force single-threaded execution
-sudo docker run \
+docker run \
     --rm \
     --publish=5432:5432 \
     --volume $PATHVAR/data:/sorting_data \
@@ -10,14 +10,12 @@ sudo docker run \
     --detach \
     umbra-image:latest
 
-sudo docker cp create-role.sql umbra-container:/create-role.sql
-
-sudo docker exec \
+docker exec \
     --interactive umbra-container /umbra/bin/sql \
     --createdb test \
     create-role.sql
 
-sudo docker exec \
+docker exec \
     --detach umbra-container \
     /umbra/bin/server -address 0.0.0.0 test
 
