043a668ea5ab8d958a29c08cbb0a4078c025212d
Laurens Kuiper <laurens.kuiper@cwi.nl>
2022-01-27 11:01:25 +0100

    jewels07 trace results

--- sorting_simulation/trace_perf.py
+++ sorting_simulation/trace_perf.py
@@ -6,7 +6,7 @@ import re
 
 
 EXPORT_CMD = 'perf script > perf.export'
-RECORD_CMD = 'perf record -e cache-misses-branch-misses ./simulation '
+RECORD_CMD = 'perf record -e cache-misses,branch-misses -F 100 ./simulation '
 
 
 def create_csv_header(csv):
@@ -15,7 +15,7 @@ def create_csv_header(csv):
 
 
 def trace(args):
-    subprocess.run(RECORD_CMD + args, shell=True, capture_output=True)
+    subprocess.run(RECORD_CMD + args, shell=True, capture_output=False)
 
 
 def append_run(category, count, columns, csv):
@@ -37,11 +37,11 @@ def main():
     key_cols = 3
     payload_cols = 1 << 5
     configurations = [
-        # ('comparator', rows, key_cols, ['col_all', 'col_ss', 'col_branchless', 'row_all', 'row_all_branchless', 'row_iter', 'row_norm']),
-        # ('sort', rows, key_cols, ['pdq_static', 'radix']),
+        ('comparator', rows, key_cols, ['col_all', 'col_ss', 'col_branchless', 'row_all', 'row_all_branchless', 'row_iter', 'row_norm']),
+        ('sort', rows, key_cols, ['pdq_static', 'radix']),
         ('merge_key', rows, key_cols, ['row_all', 'row_all_branchless', 'row_norm', 'col_branch', 'col_branchless']),
-        # ('reorder', rows, payload_cols, ['row', 'col']),
-        # ('merge_payload', rows, payload_cols, ['row', 'col'])
+        ('reorder', rows, payload_cols, ['row', 'col']),
+        ('merge_payload', rows, payload_cols, ['row', 'col'])
     ]
     for sim, count, columns, categories in configurations:
         fname = f'results/perf_output/trace_{sim}.csv'
@@ -49,7 +49,7 @@ def main():
             create_csv_header(f)
             for category in categories:
                 args = f' {sim} {category} {count} {columns}'
-                print(args)
+                # print(args)
                 trace(args)
                 append_run(category, count, columns, f)
 
