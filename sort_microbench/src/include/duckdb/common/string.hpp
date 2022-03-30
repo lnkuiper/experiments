daec67748ae78aa2645edb2c3db16372f9fc57e7
Laurens Kuiper <laurens.kuiper@cwi.nl>
2022-03-30 13:11:07 +0200

    update issue labeler

---
+++ sort_microbench/src/include/duckdb/common/string.hpp
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/common/string.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <sstream>
+#include <string>
+
+namespace duckdb {
+using std::string;
+}
