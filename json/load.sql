a83090595682ea83f2a406c9e4ebb01f619c40f9
Laurens Kuiper <laurens.kuiper@cwi.nl>
2023-06-07 14:34:03 +0200

    init oocha

--- json/load.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO customer_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/customer.json');
-INSERT INTO lineitem_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/lineitem.json');
-INSERT INTO nation_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/nation.json');
-INSERT INTO orders_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/orders.json');
-INSERT INTO part_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/part.json');
-INSERT INTO partsupp_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/partsupp.json');
-INSERT INTO region_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/region.json');
-INSERT INTO supplier_json SELECT * FROM read_json_objects('/Users/laurens/git/experiments/json/data/supplier.json');
--- json/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE customer_json (json JSON);
-CREATE TABLE lineitem_json (json JSON);
-CREATE TABLE nation_json (json JSON);
-CREATE TABLE orders_json (json JSON);
-CREATE TABLE part_json (json JSON);
-CREATE TABLE partsupp_json (json JSON);
-CREATE TABLE region_json (json JSON);
-CREATE TABLE supplier_json (json JSON);
