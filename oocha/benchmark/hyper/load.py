4bc307a8e33b544633bb66ca8dd8a78ead59abc3
Laurens Kuiper <laurens.kuiper@cwi.nl>
2023-11-16 10:24:58 +0100

    update scripts

--- oocha/benchmark/hyper/load.py
+++ oocha/benchmark/hyper/load.py
@@ -16,14 +16,14 @@ def main():
     with HyperProcess(telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU, hyper_path=hyper_path, parameters=process_parameters) as hyper:
         with Connection(hyper.endpoint, db_path, CreateMode.CREATE_IF_NOT_EXISTS) as con:
             for sf in SCALE_FACTORS:
-                con.execute_query("""START TRANSACTION;""")
-                con.execute_query(get_schema(sf, system='hyper'))
-                con.execute_query(f"""SELECT count(*) FROM lineitem{sf};""")
-                if con.fetchall()[0][0] == 0:
-                    print(f'Loading hyper SF{sf} ...')
-                    con.execute_query(f"""COPY lineitem{sf} FROM '{get_csv_path(sf)}' (FORMAT CSV, HEADER TRUE, QUOTE '"', DELIMITER ',');""")
-                    print(f'Loading hyper SF{sf} done.')
-                con.execute_query("""COMMIT;""")
+                con.execute_query("""START TRANSACTION;""").close()
+                con.execute_query(get_schema(sf, system='hyper')).close()
+                with con.execute_query(f"""SELECT count(*) FROM lineitem{sf};""") as res:
+                    if res[0][0] == 0:
+                        print(f'Loading hyper SF{sf} ...')
+                        con.execute_query(f"""COPY lineitem{sf} FROM '{get_csv_path(sf)}' (FORMAT CSV, HEADER TRUE, QUOTE '"', DELIMITER ',');""").close()
+                        print(f'Loading hyper SF{sf} done.')
+                    con.execute_query("""COMMIT;""").close()
 
 
 if __name__ == '__main__':
