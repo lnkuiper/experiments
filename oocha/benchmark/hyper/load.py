1c99e551076bd618fafde156a3045bd6f98a9d8a
Laurens Kuiper <laurens.kuiper@cwi.nl>
2023-11-16 12:46:29 +0100

    update scripts

--- oocha/benchmark/hyper/load.py
+++ oocha/benchmark/hyper/load.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import subprocess
 from tableauhyperapi import HyperProcess, Telemetry, Connection, CreateMode
 
 
--- oocha/benchmark/hyper/run.py
+++ oocha/benchmark/hyper/run.py
@@ -1,7 +1,6 @@
 import os
 import sys
-import psycopg2
-import subprocess
+from tableauhyperapi import HyperProcess, Telemetry, Connection, CreateMode
 
 
 SYSTEM_DIR = os.path.dirname(__file__)
@@ -9,24 +8,17 @@ sys.path.append(f'{SYSTEM_DIR}/..')
 from util.util import *
 
 
-def run_query(query, cur):
-    cur.execute(query)
-    return cur.fetchall()
+def run_query(query, con):
+    con.execute_query(query).close()
 
 
 def main():
-    server = subprocess.Popen(f'{SYSTEM_DIR}/hyper/hyperd --database ./hyper/mydb --log-dir . --skip-license --init-user raasveld --listen-connection tab.tcp://localhost:7484,tab.domain:///tmp/LD/domain/.s.PGSQL.7484 --no-password run'.split(' '), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    time.sleep(10)
-    try:
-        con = psycopg2.connect(database=f"{SYSTEM_DIR}/hyper/mydb", host="localhost", user="raasveld", password="", port=7484)
-        cur = con.cursor()
-        run_benchmark('hyper', run_query, cur)
-    except Exception as e:
-        my_exception = e
-    finally:
-        server.terminate()
-    raise my_exception
-
+    hyper_path = f'{SYSTEM_DIR}/hyper/'
+    db_path = f"{SYSTEM_DIR}/hyper/mydb"
+    process_parameters = {"default_database_version": "2"}
+    with HyperProcess(telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU, hyper_path=hyper_path, parameters=process_parameters) as hyper:
+        with Connection(hyper.endpoint, db_path, CreateMode.CREATE_IF_NOT_EXISTS) as con:
+            run_benchmark('hyper', run_query, con)
 
 
 if __name__ == '__main__':
