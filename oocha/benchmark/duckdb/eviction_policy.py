e6b3fd3edce39f30cbe84f37928733e09847804d
Laurens Kuiper <laurens@duckdblabs.com>
2024-02-13 10:35:55 +0000

    results with 3gb/thread

--- oocha/benchmark/duckdb/eviction_policy.py
+++ oocha/benchmark/duckdb/eviction_policy.py
@@ -8,11 +8,11 @@ sys.path.append(f'{SYSTEM_DIR}/..')
 from util.util import *
 
 
-THREAD_COUNT = 4
+THREAD_COUNTS = [1, 4]
 EVICTION_POLICY_REPETITIONS = 10
 POLLING_INTERVAL = 0.5
-POLICY = 'all'
-#POLICY = 'persistent_temporary'
+#POLICY = 'all'
+POLICY = 'persistent_temporary'
 #POLICY = 'temporary_persistent'
 
 
@@ -23,37 +23,38 @@ def run_query_loop(con):
 
 
 def main():
-    #db_path = f'{SYSTEM_DIR}/data.db'
-    db_path = '/data/data.db'
-    con = duckdb.connect(db_path, read_only=True)
-    con.execute("""SET preserve_insertion_order=false""")
-    con.execute(f"""SET memory_limit='{2 * THREAD_COUNT}GB'""") # 2 GB per concurrent reader
-    con.execute(f"""SET threads={4 * THREAD_COUNT}""") # 4 threads per concurrent reader
-
-    eviction_policy_db_path = f'{SYSTEM_DIR}/eviction_policy.db'
-    results_con = con.cursor()
-    results_con.execute(f"""ATTACH IF NOT EXISTS '{eviction_policy_db_path}' AS results (READ_ONLY FALSE)""")
-    results_con.execute("""CREATE TABLE IF NOT EXISTS results.results (policy VARCHAR, time DOUBLE, threads UTINYINT, tag VARCHAR, memory_usage_bytes BIGINT, temporary_storage_bytes BIGINT)""")
-
-    threads = []
-    for i in range(THREAD_COUNT):
-        threads.append(Thread(target=run_query_loop, args=(con,)))
+    for THREAD_COUNT in THREAD_COUNTS:
+        #db_path = f'{SYSTEM_DIR}/data.db'
+        db_path = '/data/data.db'
+        con = duckdb.connect(db_path, read_only=True)
+        con.execute("""SET preserve_insertion_order=false""")
+        con.execute(f"""SET memory_limit='{3 * THREAD_COUNT}GB'""") # 3 GB per concurrent reader
+        con.execute(f"""SET threads={4 * THREAD_COUNT}""") # 4 threads per concurrent reader
     
-    before = time.time()
-    for thread in threads:
-        thread.start()
-    while True:
-        results_con.execute(f"""INSERT INTO results.results SELECT '{POLICY}', {time.time() - before}, {THREAD_COUNT}, * FROM duckdb_memory() WHERE tag = 'BASE_TABLE' OR tag = 'HASH_TABLE'""")
-        at_least_one_alive = False
+        eviction_policy_db_path = f'{SYSTEM_DIR}/eviction_policy.db'
+        results_con = con.cursor()
+        results_con.execute(f"""ATTACH IF NOT EXISTS '{eviction_policy_db_path}' AS results (READ_ONLY FALSE)""")
+        results_con.execute("""CREATE TABLE IF NOT EXISTS results.results (policy VARCHAR, time DOUBLE, threads UTINYINT, tag VARCHAR, memory_usage_bytes BIGINT, temporary_storage_bytes BIGINT)""")
+    
+        threads = []
+        for i in range(THREAD_COUNT):
+            threads.append(Thread(target=run_query_loop, args=(con,)))
+        
+        before = time.time()
         for thread in threads:
-            if thread.is_alive():
-                at_least_one_alive = True
+            thread.start()
+        while True:
+            results_con.execute(f"""INSERT INTO results.results SELECT '{POLICY}', {time.time() - before}, {THREAD_COUNT}, * FROM duckdb_memory() WHERE tag = 'BASE_TABLE' OR tag = 'HASH_TABLE'""")
+            at_least_one_alive = False
+            for thread in threads:
+                if thread.is_alive():
+                    at_least_one_alive = True
+                    break
+            if not at_least_one_alive:
                 break
-        if not at_least_one_alive:
-            break
-        time.sleep(POLLING_INTERVAL)
-    for thread in threads:
-        thread.join()
+            time.sleep(POLLING_INTERVAL)
+        for thread in threads:
+            thread.join()
 
 
 if __name__ == '__main__':
