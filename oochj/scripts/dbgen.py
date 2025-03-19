c34b54d4d59c91a04a64aedbfbdb19537894cc24
Laurens Kuiper <laurens.kuiper@cwi.nl>
2025-03-19 10:57:43 +0100

    update latest notebooks and plots etc

--- oochj/scripts/dbgen.py
+++ oochj/scripts/dbgen.py
@@ -1,13 +1,9 @@
+import argparse
 import os
 import duckdb
 import multiprocessing
 import shutil
 import sys
-
-
-SCRIPTS_DIR = os.path.dirname(__file__)
-sys.path.append(f'{SCRIPTS_DIR}/..')
-from benchmark.util.util import *
 
 
 TABLES = [
@@ -29,6 +25,7 @@ def task(arg_dict):
     con = duckdb.connect(temporary_db)
     con.execute("SET threads=1;")
     con.execute("SET memory_limit='1gb';")
+    con.execute("PRAGMA disable_progress_bar;")
     for step in range(arg_dict.get('step_start'), arg_dict.get('step_end')):
         con.execute(f"CALL dbgen(sf={arg_dict.get('sf')}, children={arg_dict.get('total_steps')}, step={step});")
     con.close()
@@ -53,15 +50,12 @@ def generate_sf(sf):
     with multiprocessing.Pool(cpu_count) as p:
         p.map(task, tasks)
 
-    temporary_db = 'temp.db'
+    temporary_db = f'tpch-sf{sf}.db'
     if os.path.exists(temporary_db):
         os.remove(temporary_db)
-    con = duckdb.connect('temp.db')
+    con = duckdb.connect(temporary_db)
 
-    con.execute("SET preserve_insertion_order=false;")
-    # con.execute("SET allocator_background_threads=true;")
     con.execute("CALL dbgen(sf=0);")
-
     for i in range(cpu_count):
         con.execute(f"ATTACH 'temp{i}.db' AS temp{i} (READ_ONLY);")
         con.execute("START TRANSACTION;")
@@ -70,31 +64,17 @@ def generate_sf(sf):
         con.execute("COMMIT;")
         con.execute(f"DETACH temp{i};")
         os.remove(f'temp{i}.db')
-
-    output = f'{DATA_DIR}/sf{sf}'
-    if os.path.exists(output):
-        shutil.rmtree(output)
-    con.execute(f"EXPORT DATABASE '{output}';")
-
     con.close()
-    os.remove(temporary_db)
-
-    with open(f"{output}/load.sql", 'r') as f:
-        header_true = f.read().replace('header 1', 'header TRUE')
-    with open(f"{output}/load.sql", 'w') as f:
-        f.write(header_true)
 
 def main():
-    if not os.path.exists(DATA_DIR):
-        os.mkdir(DATA_DIR)
+    parser = argparse.ArgumentParser(prog='dbgen.py', description='Generates TPC-H at the given SF in parallel using DuckDB', epilog='Example: python3 dbgen.py 10')
+    parser.add_argument('sf')
+    args = parser.parse_args()
 
-    for sf in SCALE_FACTORS:
-        output = f'{DATA_DIR}/sf{sf}'
-        if os.path.exists(output):
-            continue
-        print(f'Generating SF{sf} ...')
-        generate_sf(sf)
-        print(f'Generating SF{sf} done.')
+    sf = int(args.sf)
+    print(f'Generating SF{sf} ...')
+    generate_sf(sf)
+    print(f'Generating SF{sf} done.')
 
 
 if __name__ == '__main__':
